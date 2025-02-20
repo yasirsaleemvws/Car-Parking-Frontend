fd099b428bdec27fbc3be183dcdfedaee1ca91b5
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2025-02-20 17:12:52 +0500

    Update MembershipType.jsx

--- src/components/modals/MembershipType.jsx
+++ src/components/modals/MembershipType.jsx
@@ -6,8 +6,6 @@ import { PiExcludeFill } from "react-icons/pi";
 
 export default function MembershipTypeModal({ visible, onClose, onSelect, value }) {
     const [selectedType, setSelectedType] = React.useState(value);
-    console.log("::::::",value);
-    
 
     const handleConfirm = () => {
         onSelect(selectedType);
@@ -26,32 +24,32 @@ export default function MembershipTypeModal({ visible, onClose, onSelect, value 
                 <h2 className="text-lg font-semibold mb-6">Select Your Plan</h2>
                 <Radio.Group onChange={e => setSelectedType(e.target.value)} className="w-full">
                     <div className="flex flex-col space-y-4">
-                        <div className="flex items-center justify-between p-4 gap-2 border rounded-lg">
-                            <IoLayersSharp className='text-xl' />
+                        <div className={`flex items-center justify-between p-4 gap-2 border rounded-lg ${selectedType === 'Basic Monthly' ? 'border-purple-800 border-2' : ''}`}>
+                            <IoLayersSharp className={`text-2xl ${selectedType === 'Basic Monthly' ? 'text-purple-800' : ''}`} />
                             <div>
                                 <span className="font-medium">Basic Monthly</span>
                                 <span className="text-sm text-gray-600 mt-1">$300/month</span>
                                 <p className="text-sm text-gray-500">Lorem Ipsum has been the industry's standard dummy</p>
                             </div>
-                            <Radio value="Basic Monthly" />
+                            <Radio value="Basic Monthly" checked={selectedType === 'Basic Monthly'} />
                         </div>
-                        <div className="flex items-center justify-between p-4 gap-2 border rounded-lg">
-                            <MdOutlineLayers className='text-xl' />
+                        <div className={`flex items-center justify-between p-4 gap-2 border rounded-lg ${selectedType === 'Free' ? 'border-purple-800 border-2' : ''}`}>
+                            <MdOutlineLayers className={`text-2xl ${selectedType === 'Free' ? 'text-purple-800' : ''}`} />
                             <div>
                                 <span className="font-medium">Free</span>
                                 <span className="text-sm text-gray-600 mt-1">$0/month</span>
                                 <p className="text-sm text-gray-500">Lorem Ipsum has been the industry's standard dummy</p>
                             </div>
-                            <Radio value="Free" />
+                            <Radio value="Free" checked={selectedType === 'Free'} />
                         </div>
-                        <div className="flex items-center justify-between p-4 gap-2 border rounded-lg">
-                            <PiExcludeFill className='text-xl' />
+                        <div className={`flex items-center justify-between p-4 gap-2 border rounded-lg ${selectedType === 'Basic Yearly' ? 'border-purple-800 border-2' : ''}`}>
+                            <PiExcludeFill className={`text-2xl ${selectedType === 'Basic Yearly' ? 'text-purple-800' : ''}`} />
                             <div>
                                 <span className="font-medium">Basic Yearly</span>
                                 <span className="text-sm text-gray-600 mt-1">$40/month</span>
                                 <p className="text-sm text-gray-500">Lorem Ipsum has been the industry's standard dummy</p>
                             </div>
-                            <Radio value="Basic Yearly" />
+                            <Radio value="Basic Yearly" checked={selectedType === 'Basic Yearly'} />
                         </div>
                     </div>
                 </Radio.Group>
