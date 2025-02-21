46218261c3862316dcb0fd49ec96f06d3518e0ae
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2025-02-21 15:25:40 +0500

    udpated

--- src/pages/parking-section/parking-area/ParkingArea.jsx
+++ src/pages/parking-section/parking-area/ParkingArea.jsx
@@ -17,6 +17,16 @@ export default function ParkingArea() {
     const view = query.get('view');
     const [floor, setFloor] = React.useState(3);
 
+    const redirect = (route) => {
+        if (route == '2d') {
+            navigate(`${APP_ROUTES.PARKING_AREA}?id=${id}&&view=2d`)
+        } else if (route == '3d') {
+            setFloor(3)
+            navigate(`${APP_ROUTES.PARKING_AREA}?id=${id}&&view=3d`)
+        } else if (route == 'camera') {
+            navigate(`${APP_ROUTES.PARKING_CAMERA}?id=${id}`)
+        }
+    }
 
     return (
         <>
@@ -25,22 +35,22 @@ export default function ParkingArea() {
                 <div className="flex items-center gap-2">
                     {view == '3d' ? (
                         <>
-                            <select className='mr-2 px-4 h-[40px] rounded-md border border-gray-400' onChange={() => setFloor(parseInt(event.target.value))}>
+                            <select value={floor} className='mr-2 px-4 h-[40px] rounded-md border border-gray-400' onChange={() => setFloor(parseInt(event.target.value))}>
                                 <option value="3">Top Floor</option>
                                 <option value="2">Second Floor</option>
                                 <option value="1">Ground Floor</option>
                             </select>
-                            <button className='flex items-center bg-white text-purple-700 px-4 h-[40px] rounded-md border border-gray-400' onClick={() => navigate(`${APP_ROUTES.PARKING_AREA}?id=${data.id}&&view=2d`)}>
+                            <button className='flex items-center bg-white text-purple-700 px-4 h-[40px] rounded-md border border-gray-400' onClick={() => redirect('2d')}>
                                 <DiHtml53dEffects className='text-2xl mr-2' /> 2D View
                             </button>
                         </>
                     ) : (
-                        <button className='flex items-center bg-white text-purple-700 px-4 h-[40px] rounded-md border border-gray-400' onClick={() => navigate(`${APP_ROUTES.PARKING_AREA}?id=${data.id}&&view=3d`)}>
+                        <button className='flex items-center bg-white text-purple-700 px-4 h-[40px] rounded-md border border-gray-400' onClick={() => redirect('3d')}>
                             <DiHtml53dEffects className='text-2xl mr-2' /> 3D View
                         </button>
                     )}
 
-                    <button className='flex items-center bg-purple-700 text-white px-4 h-[40px] rounded-md ml-2 border border-gray-400' onClick={() => navigate(`${APP_ROUTES.PARKING_CAMERA}?id=${data.id}`)}>
+                    <button className='flex items-center bg-purple-700 text-white px-4 h-[40px] rounded-md ml-2 border border-gray-400' onClick={() => redirect('camera')}>
                         <FaCamera className='text-lg mr-2' />Camera View
                     </button>
                 </div>
--- src/pages/parking-section/parking-area/view-3D/View3D.jsx
+++ src/pages/parking-section/parking-area/view-3D/View3D.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function View3D({ floor }) {
     return (
         <div className='min-h-[70vh] flex items-center justify-center'>
-            {floor == '3'? (
+            {floor == '3' ? (
                 <img src="/images/floor3.png" alt="" className='max-w-[700px]' />
-            ):floor == '2'?(
+            ) : floor == '2' ? (
                 <img src="/images/floor2.png" alt="" className='max-w-[700px]' />
-            ):(
+            ) : (
                 <img src="/images/floor1.png" alt="" className='max-w-[700px]' />
             )}
         </div>
